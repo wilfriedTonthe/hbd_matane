dc1f0858ccc97476fb9e3380fefcfdb4a5e2ec7e
wilfriedTonthe <willytonthe1@gmail.com>
2025-02-11 16:08:32 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base:'/site_de_HBD/',
   plugins: [react()],
+ base: "/site_de_HBD/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
