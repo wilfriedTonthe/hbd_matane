e39bb2994712513ea362bfae3048554bed101aff
wilfriedTonthe <willytonthe1@gmail.com>
2025-02-11 19:12:11 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/site_de_HBD/",
+  base: "./",
   build: {
     outDir: "dist",
     rollupOptions: {
